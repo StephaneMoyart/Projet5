dd4debb07da1ac32b27064e18c05d290ed5e2501
stephaneM <moyartstephane@gmail.com>
2024-10-12 14:54:18 +0200

    responsive

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './Style/index.scss'
+import './Style/style.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
